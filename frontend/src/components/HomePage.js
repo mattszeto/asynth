88430c4103501793ac96974ff219d311d9688015
mattszeto <ms8288@gmail.com>
2021-01-20 15:33:04 -0800

    created room page, join page, create room page

---
+++ frontend/src/components/HomePage.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import {
+  BrowserRouter as Router,
+  Link,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+import RoomJoinPage from "./RoomJoinPage";
+import CreateRoomPage from "./CreateRoomPage";
+import Room from "./Room";
+
+export default class HomePage extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <h1>Home Page</h1>
+          </Route>
+          <Route path="/join" component={RoomJoinPage}></Route>
+          <Route path="/create" component={CreateRoomPage}></Route>
+          <Route path="/room/:roomCode" component={Room}></Route>
+        </Switch>
+      </Router>
+    );
+  }
+}
